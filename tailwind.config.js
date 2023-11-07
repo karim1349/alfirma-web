2f37fb9e6a93cc221dd20b084550a2e33ddc5d8d
Karim Benchekroun <karimbenchekroun@outlook.fr>
2023-11-07 11:43:04 +0100

    colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        "purple-350": "#D0C4E2",
+        "purple-550": "#9184A4",
+        "purple-750": "#6A38AF",
+        "purple-850": "#4F1F7A",
         "purple-950": "#24094A",
+        "blue-950": "#190732",
       },
     },
   },
