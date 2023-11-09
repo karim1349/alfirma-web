872e688c2cf8472a520a23d9f3c3d2f8039329a0
Karim Benchekroun <karimbenchekroun@outlook.fr>
2023-11-09 17:10:44 +0100

    email sending fully working

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import { HashRouter } from "react-router-dom";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from './pages/Home/Home';
 import PrivacyDuodoku from './pages/Duodoku/Privacy';
+import axios from 'axios';
 
+axios.defaults.baseURL = 'http://127.0.0.1:8000' //'https://alfirma-api-3efa34f27f01.herokuapp.com'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
--- src/pages/Home/components/ContactForm.js
+++ src/pages/Home/components/ContactForm.js
@@ -1,12 +1,20 @@
 import { useState } from "react"
-
+import axios from 'axios'
 const ContactForm = () => {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [message, setMessage] = useState('')
-
+    const [isSent, setIsSent] = useState(false)
     const sendEmail = () => {
-        console.log(name, email, message)
+        axios.post('/api/message/', {
+            name:name,
+            email:email,
+            message:message
+        })
+        setIsSent(true)
+        setName('')
+        setEmail('')
+        setMessage('')
     }
 
     return (
@@ -15,21 +23,29 @@ const ContactForm = () => {
             <div className='my-12 flex flex-col flex-1 w-full px-4 md:max-w-[50%]'>
                 <h2 id="SECTION_CONTACT" className='text-white font-extrabold text-2xl text-center tracking-widest px-8'>NOUS CONTACTER</h2>
                 <p className='text-gray-300 my-4 text-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-                <div className='py-2'>
-                    <label className='text-white font-bold text-sm tracking-widest'>Nom</label>
-                    <input className='bg-purple-550 w-full py-2 px-4 my-2 rounded-md outline-none text-purple-750 focus:bg-purple-350' onChange={(e) => setName(e.target.value)} />
-                </div>
-                <div className='py-2'>
-                    <label className='text-white font-bold text-sm tracking-widest'>E-mail</label>
-                    <input className='bg-purple-550 w-full py-2 px-4 my-2 rounded-md outline-none text-purple-750 focus:bg-purple-350' onChange={(e) => setEmail(e.target.value)} />
-                </div>
-                <div className='py-2'>
-                    <label className='text-white font-bold text-sm tracking-widest'>Message</label>
-                    <textarea className='bg-purple-550 w-full py-2 px-4 my-2 rounded-md outline-none text-purple-750 focus:bg-purple-350 h-56' onChange={(e) => setMessage(e.target.value)}  />
-                </div>
-                <button className='bg-purple-850 px-24 py-4 rounded-md self-center my-4 disabled:opacity-50' disabled={name === '' || email === '' || message === ''} onClick={sendEmail}>
-                    <span className='text-white font-semibold text-md'>Envoyer</span>
-                </button>
+                <form className='flex flex-col' onSubmit={sendEmail}>
+                    <div className='py-2'>
+                        <label className='text-white font-bold text-sm tracking-widest'>Nom</label>
+                        <input type="text" className='bg-purple-550 w-full py-2 px-4 my-2 rounded-md outline-none text-purple-750 focus:bg-purple-350' onChange={(e) => setName(e.target.value)} />
+                    </div>
+                    <div className='py-2'>
+                        <label className='text-white font-bold text-sm tracking-widest'>E-mail</label>
+                        <input type="email" className='bg-purple-550 w-full py-2 px-4 my-2 rounded-md outline-none text-purple-750 focus:bg-purple-350' onChange={(e) => setEmail(e.target.value)} />
+                    </div>
+                    <div className='py-2'>
+                        <label className='text-white font-bold text-sm tracking-widest'>Message</label>
+                        <textarea type="text" className='bg-purple-550 w-full py-2 px-4 my-2 rounded-md outline-none text-purple-750 focus:bg-purple-350 h-56' onChange={(e) => setMessage(e.target.value)}  />
+                    </div>
+                    {
+                        isSent &&
+                        <div className="border-white border rounded-lg my-4">
+                            <p className="text-white text-center p-2">Message bien reçu ! Nous reviendrons vers vous dans les plus brefs délais. </p>
+                        </div>
+                    }
+                    <button type="submit" className='bg-purple-850 px-24 py-4 rounded-md self-center my-4 disabled:opacity-50' disabled={name === '' || email === '' || message === ''}>
+                        <span className='text-white font-semibold text-md'>Envoyer</span>
+                    </button>
+                </form>
             </div>
         </div>
     )
