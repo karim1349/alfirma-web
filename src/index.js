febb1acc6076e2f6c641ffa13a64c7378118bcb4
Karim Benchekroun <karimbenchekroun@outlook.fr>
2024-02-08 16:08:05 +0100

    useless imports removed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { HashRouter } from "react-router-dom";
-import { Routes, Route, Navigate } from "react-router-dom";
-import Home from './pages/Home/Home';
-import PrivacyDuodoku from './pages/Duodoku/Privacy';
 import axios from 'axios';
 import Footer from './components/Footer';
 import App from './App';
