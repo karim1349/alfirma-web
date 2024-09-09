5001c6fe96cc5abc300b0864bab27285b16d3df5
Karim Benchekroun <karimbenchekroun@outlook.fr>
2024-09-09 15:04:23 +0100

    links added to cards

--- src/pages/Home/components/Card.js
+++ src/pages/Home/components/Card.js
@@ -1,15 +1,24 @@
 import { useSwiper } from 'swiper/react';
 
-const Card = ({image, title, text, review}) => {
+const Card = ({image, title, text, review, links}) => {
     const swiper = useSwiper();
     return (
         <div className=" bg-white flex flex-1 self-center flex-col rounded-2xl shadow-md m-8 h-96 p-4">
-            <div className='flex flex-1 flex-col overflow-hidden '>
+            <div className='flex flex-1 flex-col overflow-y-auto overflow-x-hidden'>
                 <img src={image} alt={title} className="h-16 object-contain z-10" />
-                <h1 className='text-center mb-3 text-xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-rose-400 to-blue-400'>{title}</h1>
-                <p className="text-xs font-light text-center">{text}</p>
+                <h1 className='text-center text-xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-rose-400 to-blue-400'>{title}</h1>
+                {links?.length &&
+                    <div className='flex flex-col sm:flex-row justify-center items-center my-2'>
+                        {links.map((link, index) => (
+                            <a href={link.url} target="_blank" rel="noreferrer" key={index}>
+                                <img src={`/assets/button_${link.type}.png`} alt={link.name} className="w-32 m-2 hover:scale-110 transform transition" />  
+                            </a>
+                        ))}
+                    </div>
+                }
+                <p className="text-xs font-light text-center text-ellipsis">{text}</p>
                 {review && 
-                    <p className="text-xs font-semibold italic my-2 text-center">"{review}"</p>
+                    <p className="text-xs font-semibold italic my-2 text-center text-ellipsis">"{review}"</p>
                 }
             </div>
             <div className='flex justify-self-end justify-between items-center'>
