9e66c23571f3d3e7ff16390f6b40b671d7b45ad0
Karim Benchekroun <karimbenchekroun@outlook.fr>
2025-09-29 23:32:51 +0200

    universal links

--- src/app/duodoku/game/layout.tsx
+++ src/app/duodoku/game/layout.tsx
@@ -5,34 +5,35 @@ export async function generateMetadata({ params }: { params: Promise<{ id: strin
     const gameId = resolvedParams.id;
     
     return {
-        title: `Rejoignez la partie #${gameId} - Duodoku`,
+        title: `Rejoignez la partie - Duodoku`,
         description: `Vous avez été invité à rejoindre une partie de Sudoku multi-joueurs sur Duodoku ! Téléchargez l'application et affrontez d'autres joueurs en temps réel.`,
         openGraph: {
-            title: `Rejoignez la partie #${gameId} - Duodoku`,
+            title: `Rejoignez la partie - Duodoku`,
             description: `Vous avez été invité à rejoindre une partie de Sudoku multi-joueurs sur Duodoku ! Téléchargez l'application et affrontez d'autres joueurs en temps réel.`,
             images: [
                 {
                     url: '/assets/duodoku.png',
                     width: 1200,
                     height: 630,
-                    alt: `Duodoku - Partie #${gameId}`,
+                    alt: `Duodoku - Partie Duo`,
                 }
             ],
             type: 'website',
-            url: `https://www.alfirma.com/duodoku/game/${gameId}`,
+            url: `https://www.alfirma.com/duodoku/game?id=30`,
         },
         twitter: {
             card: 'summary_large_image',
-            title: `Rejoignez la partie #${gameId} - Duodoku`,
+            title: `Rejoignez la partie - Duodoku`,
             description: `Vous avez été invité à rejoindre une partie de Sudoku multi-joueurs sur Duodoku ! Téléchargez l'application et affrontez d'autres joueurs en temps réel.`,
             images: ['/assets/duodoku.png'],
         },
         other: {
             'og:image:width': '1200',
             'og:image:height': '630',
+            'apple-itunes-app': 'app-id=6448099658'
         },
         alternates: {
-            canonical: `https://www.alfirma.com/duodoku/game/${gameId}`
+            canonical: `https://www.alfirma.com/duodoku/game?id=30`
         }
     };
 }
