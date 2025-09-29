14eab487d3865025dd5ebaceafd75f416bbdac3f
Karim Benchekroun <karimbenchekroun@outlook.fr>
2025-09-29 14:53:02 +0200

    migration fixes

--- src/app/home/components/Card.tsx
+++ src/app/home/components/Card.tsx
@@ -8,15 +8,15 @@ const Card = ({image, title, text, review, links}: {image: string, title: string
             <div className='flex flex-1 flex-col overflow-y-auto overflow-x-hidden'>
                 <img src={image} alt={title} className="h-16 object-contain z-10" />
                 <h1 className='text-center text-xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-rose-400 to-blue-400'>{title}</h1>
-                {links?.length &&
+                {links?.length ?
                     <div className='flex flex-col sm:flex-row justify-center items-center my-2'>
                         {links.map((link, index) => (
                             <a href={link.url} target="_blank" rel="noreferrer" key={index}>
                                 <img src={`/assets/button_${link.type}.png`} className="w-32 m-2 hover:scale-110 transform transition" />  
                             </a>
                         ))}
-                    </div>
-                }
+                    </div> 
+                : null}
                 <p className="text-xs font-light text-center text-ellipsis">{text}</p>
                 {review && 
                     <p className="text-xs font-semibold italic my-2 text-center text-ellipsis">"{review}"</p>
